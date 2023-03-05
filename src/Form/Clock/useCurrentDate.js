fcc3e7b2105f005a76b668f442fd6902afc3fa7f
Przemyslaw Gajczewski <przemek0000przemek@gmail.com>
2023-03-05 17:57:12 +0100

    used GET method to read currencies from server, check status 200 and compare with localStorage

--- src/Form/Clock/useCurrentDate.js
+++ src/Form/Clock/useCurrentDate.js
@@ -1,7 +1,27 @@
 import { useState, useEffect } from "react";
 
+const getCurrencies = async () => {
+    try {
+        const response = await fetch("https://api.exchangerate.host/latest?base=PLN");
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+        const data = await response.json();
+        localStorage.setItem("data", JSON.stringify(data))
+    } catch(error) {
+        console.error("Ups, sprawdź połączenie z internetem lub serwer chwilowo niedostępny :(");
+    }
+}
+
 const useCurrentDate = () => {
     const [date, setDate] = useState(new Date());
+    const [data, setData] = useState(JSON.parse(localStorage.getItem("data")) || {})
+
+    useEffect(() => {
+        if (!localStorage.getItem("data")) {
+            getCurrencies();
+        }
+    }, [])
 
     useEffect(() => {
         const intervalId = setInterval(() => {
