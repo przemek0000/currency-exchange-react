cc139e6753307ef180f4c15caf20df7d6e27937b
Przemyslaw Gajczewski <przemek0000przemek@gmail.com>
2023-03-08 12:53:06 +0100

    added active button brighteness

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -23,9 +23,9 @@ export const StyledError = styled.div`
     align-items: center;
     justify-content: center;
     background-color: hsl(162, 100%, 90%);
-    border: 5px solid ${({color}) => color === "red" ? color : "rgb(0, 155, 108)"};
+    border: 5px solid ${({ color }) => color === "red" ? color : "rgb(0, 155, 108)"};
     border-radius: 10px;
-    color: ${({color}) => color};
+    color: ${({ color }) => color};
 `
 
 export const StyledLegend = styled.legend`
@@ -44,36 +44,41 @@ export const StyledFieldset = styled.fieldset`
 `;
 
 export const StyledSpan = styled.span`
-animation: ${insideFormAnimation} 0.5s cubic-bezier(1, 0, 0.73, 0.72);
+    animation: ${insideFormAnimation} 0.5s cubic-bezier(1, 0, 0.73, 0.72);
     width: 210px;
     display: inline-block;
 `;
 
 export const StyledSelect = styled.select`
-animation: ${insideFormAnimation} 0.5s cubic-bezier(1, 0, 0.73, 0.72);
+    animation: ${insideFormAnimation} 0.5s cubic-bezier(1, 0, 0.73, 0.72);
     width: 300px;
     padding: 2px;
     border-radius: 10px;
 `;
 
 export const StyledInput = styled.input`
-animation: ${insideFormAnimation} 0.5s cubic-bezier(1, 0, 0.73, 0.72);
+    animation: ${insideFormAnimation} 0.5s cubic-bezier(1, 0, 0.73, 0.72);
     width: 300px;
     border-radius: 10px;
     padding: 5px;
 `;
 
 export const StyledButton = styled.button`
-animation: ${insideFormAnimation} 0.5s cubic-bezier(1, 0, 0.73, 0.72);
+    animation: ${insideFormAnimation} 0.5s cubic-bezier(1, 0, 0.73, 0.72);
+    transition: 0.2s;
     padding: 10px;
     width: 100%;
     border-radius: 10px;
     border: 3px solid green;
     background-color: rgba(0, 126, 0, 0.301);
+
+    &:active {
+        filter: brightness(130%);
+    }
 `;
 
 export const StyledDate = styled.p`
-animation: ${insideFormAnimation} 0.5s cubic-bezier(1, 0, 0.73, 0.72);
+    animation: ${insideFormAnimation} 0.5s cubic-bezier(1, 0, 0.73, 0.72);
     font-weight: 100;
     font-family: monospace;
     text-align: center;
